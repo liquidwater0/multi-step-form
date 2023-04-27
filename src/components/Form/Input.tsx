f373bc2d5fe04163f1e4136ad8d52fc05dae4faa
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-04-27 13:19:44 -0500

    validation messages for info inputs

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -8,11 +8,14 @@ type InputProps = {
 
 export default function Input({ type, className, required, ...props }: InputProps) {
     return (
-        <input 
-            className={`form-input  ${className ? className : ""}`}
-            type={type}
-            required
-            { ...props }
-        />
+        <div className='form-input-container'>
+            <input 
+                className={`form-input  ${className ? className : ""}`}
+                type={type}
+                required
+                { ...props }
+            />
+            <div className="validation-message">This field is required</div>
+        </div>
     );
 }
